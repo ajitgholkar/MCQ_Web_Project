4c6faa2700d50379d2f3ccf1d0716a4fb4a6ae9b
singhvikramjeet786 <singhvikramjeet786@gmail.com>
2022-04-20 02:44:36 +0530

    added complete login functionality with registions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,72 @@
+/**
+ * method to read quesbank data
+ */
 function readQuesBankData(){
-	
-	//$.getJSON("./assets/json/ques_bank.json",function(data){
-	//	console.log(data);
-	//});
 	var quesBankJSONData = quesBankData;
 	console.log("My QuesBank Data",quesBankJSONData);
-	
-	
 }
 
+/**
+ * method to login by checking user provided credentials with stored data
+ */
 function login(){
 	var userJSONData = userData;
-	console.log("UserData",userJSONData);
 	var userName = document.getElementById("userName").value;
 	var password= document.getElementById("password").value;
 	console.log("entered Password :",password);
 	console.log("entered userName :",userName);
-	
-	userJSONData.forEach(userData => {
+	if(window.localStorage.getItem('user_data')){
+		var userDataArray = JSON.parse(window.localStorage.getItem('user_data'));
+	}
+	else{
+		var userDataArray = userJSONData;
+	}
+	console.log("UserData",userDataArray);
+	userDataArray.forEach(userData => {
 		if(userName == userData.user_name && password == userData.password){
 			location.href = './dashboard.html';
-			console.log("in if:",userData.user_name);
 		}
 	});
 }
 
+/**
+ * method to go to registration page
+ */
 function gotoRegistrationPage(){
 	location.href = './registrationPage.html';
+}
+
+/**
+ * method to go to login page
+ */
+function gotologinPage(){
+	location.href = './loginPage.html';
+}
+
+/**
+ * method to save new user in local
+ */
+function savetolocal(){
+	var userName = document.getElementById("fullName").value;
+	var email= document.getElementById("email").value;
+	var password= document.getElementById("password").value;
+	var verifyHint= document.getElementById("verify").value;
+
+	console.log(userName,email,password,verifyHint);
+	var newUserData = {
+		"user_name": userName,
+		"password": password,
+		"email": email,
+		"phone": "",
+		"security_ques": verifyHint
+	};
+	if(window.localStorage.getItem('user_data')){
+		var userDataArray = JSON.parse(window.localStorage.getItem('user_data'));
+	}
+	else {
+		var userDataArray = userData;
+	}
+	userDataArray.push(newUserData)
+	window.localStorage.setItem("user_data",JSON.stringify(userDataArray));
+	location.href = './loginPage.html';
 }
